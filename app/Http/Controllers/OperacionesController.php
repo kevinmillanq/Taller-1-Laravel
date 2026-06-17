db3d954f3f9cb0db705de380da33367b75deb75f
Millan <Millan11177@gmail.com>
2026-06-16 21:14:57 -0500

    Guardar Base de Datos

--- app/Http/Controllers/OperacionesController.php
+++ app/Http/Controllers/OperacionesController.php
@@ -6,7 +6,27 @@ use Illuminate\Http\Request;
 
 class OperacionesController extends Controller
 {
-     public function multiplicar($num1, $num2)
+
+   public function frm_sumar()
+    {
+
+        return view('frm_sumar');
+
+    }
+
+    public function sumar(Request $request){
+
+    $numero1=$request->numero1;
+    $numero2=$request->numero2;
+
+    $resultado=$numero1+$numero2;
+
+    return $resultado;
+
+    }
+
+
+return resultado             public function frm_multiplicar()
     {
 
         $resultado = $num1 * $num2;
